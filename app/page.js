8d4ce971ab0dc75c9ce1c64e249f788e33265915
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2024-05-16 22:12:12 +0530

    changed title

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-8">
       <h1 className="font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center gradient-text">
-        Thief detection alarm
+        AI Object detector
       </h1>
       <ObjectDetection />
     </main>
